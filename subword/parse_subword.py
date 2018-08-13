624ba8c89257498332804155f03acf31d380aef3
Madjura <madjura@hotmail.com>
2018-08-13 12:39:21 +0200

    ngram work

--- subword/parse_subword.py
+++ subword/parse_subword.py
@@ -4,7 +4,7 @@ from parse.parse import parse_all, vokale, konsonanten
 from parse.util import match_recursion, match_recursion2
 
 
-def parse_theutonista_to_ngrams(words, ngram_start=3, ngram_end=6):
+def parse_theutonista_to_ngrams(words, ngram_start=3, ngram_end=3):
     # TODO: needs to be a list of theutonista WORDS, not LINES
     ngrams_d = {x: [] for x in range(ngram_start, ngram_end+1)}
     ngrams_done = {x: [] for x in range(ngram_start, ngram_end+1)}
@@ -47,6 +47,40 @@ def parse_theutonista_to_ngrams(words, ngram_start=3, ngram_end=6):
         pos_to_chars = dict(pos_to_chars)
         ngrams_add = list(set(ngrams_add))
         done = []
+        last = max(list(pos_to_chars.keys()))
+        firsts = []
+        lasts = []
+        for n in ngrams_add:
+            nn = n[0]  # first char
+            if nn in pos_to_chars[0]:
+                firsts.append(n)
+            if nn in pos_to_chars[last]:
+                lasts.append(n)
+        current = 1
+        while current <= last:
+            for ngram_size, ngrams in ngrams_d.items():
+                if ngrams and len(ngrams[0]) == ngram_size:
+                    # full, move
+                    new_sublists = []
+                    for sublist in ngrams:
+                        ngrams_done[ngram_size].append("".join(sublist))
+                        new_sublists.append(sublist[1:])
+                    ngrams_d[ngram_size] = new_sublists
+                    if current == last:
+                        for sublist in new_sublists:
+                            sublist.append(">")
+                            ngrams_done[ngram_size].append("".join(sublist))
+                        break
+                else:
+                    # check empty
+                    if not ngrams:
+                        for f in firsts:
+                            ngrams_d[ngram_size].append(["<", f"||{f}"])
+                    else:
+                        for sublist in ngrams:
+                            for char in pos_to_chars[current]:
+                                sublist.append(char)
+                        current += 1
         for ngram_size, ngrams in ngrams_d.items():
             if ngrams and len(ngrams[0]) == ngram_size:
                 ngrams_done[ngram_size].append("".join(ngrams))
